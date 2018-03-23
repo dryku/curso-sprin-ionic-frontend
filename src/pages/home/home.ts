430b848d241f857ac28c81167e8cf54578258f3f
Dryku <dryku@hotmail.com>
2018-03-23 02:12:24 -0300

    Navegação da tela de Categorias

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { CategoriasPage } from '../categorias/categorias';
+import { FormularioPage } from '../formulario/formulario';
 
 @IonicPage()
 @Component({
@@ -10,6 +12,15 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  login() {
+   // console.log("Olá estou no console...")
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
+  openFormulario() {
+    this.navCtrl.setRoot('FormularioPage');
   }
 
 }
